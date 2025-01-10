3e906e73a0795c8ee5b5271b217650d11fae61bd
vaishnav-mv <vaishnavam02@gmail.com>
2025-01-10 20:29:55 +0530

    cookie removed

--- app.js
+++ app.js
@@ -35,11 +35,11 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
   
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24, // 24 hours
-        secure: process.env.NODE_ENV === 'production',
-        httpOnly: true,
-    },
+    // cookie: {
+    //     maxAge: 1000 * 60 * 60 * 24, // 24 hours
+    //     secure: process.env.NODE_ENV === 'production',
+    //     httpOnly: true,
+    // },
     
 }));
 
